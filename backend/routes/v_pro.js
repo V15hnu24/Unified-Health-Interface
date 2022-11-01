1ddad471069edba3b54c4c2c5f5c10241c3f8396
Vedant <87467646+9pingg@users.noreply.github.com>
2022-11-01 22:41:11 +0530

    Add files via upload

--- backend/routes/v_pro.js
+++ backend/routes/v_pro.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { updateProfessional,deleteProfessional,getProfessional,getAllVerifiedProfessionals,getAllRejectedProfessionals,getAllPendingforApproval_Professionals,getAllProfessionals} = require('../controllers/professional');
+const { updateProfessional,deleteProfessional,getProfessional,getAllVerifiedProfessionals,getAllRejectedProfessionals,getAllPendingforApproval_Professionals,getAllProfessionals,updateDocumentAccess, getAlldocuments, getDocument} = require('../controllers/professional');
 const router =  express.Router();
-const {verifyToken, verifyUser, verifyAdmin}  = require('../utils/verifyToken');
+const {verifyToken, verifyUser, verifyAdmin, verifyDocumentAccess}  = require('../utils/verifyToken');
 router.put("/:id",verifyUser, updateProfessional);
 
 //Delete
@@ -14,9 +14,12 @@ router.get("/:id",verifyUser, getProfessional);
 router.get("/rejected_professionals", verifyAdmin, getAllRejectedProfessionals);
 
 //GetAll Verified patients
-router.get("/verified_professionals", verifyAdmin, getAllProfessionals);
+router.get("/verified_professionals", verifyAdmin, getAllVerifiedProfessionals);
 
 //GetAll pending for verfication patients
 router.get("/verifyPending_professionals", verifyAdmin, getAllPendingforApproval_Professionals);
 
+router.get("/professional_documents/:id", verifyUser, getAlldocuments);
+router.post("/documents/:id", verifyUser, updateDocumentAccess);
+router.get("/professional_document/:id", verifyDocumentAccess, getDocument);
 module.export = router;
